8d781e741b02bbe1b5463a525c27a3f04db6b2df
Jannik Rasch <147390841+RaschJ@users.noreply.github.com>
2023-10-19 13:37:48 +0200

    Update main.go

--- webserver/main.go
+++ webserver/main.go
@@ -27,6 +27,6 @@ func main() {
 
 func usage() {
 	//fmt.Fprintf(os.Stderr, "Usage of %s: [-p port] dir_to_serve\n", os.Args[0])
-	fmt.print("hello world! ._. \n")
+	fmt.Printf("hello world! ._. \n")
 	flag.PrintDefaults()
 }
