6d5872a6ba24cf4bb25e7e9ecc3df939cdf764c3
Jannik Rasch <147390841+RaschJ@users.noreply.github.com>
2023-10-19 10:44:05 +0200

    Update main.go

--- webserver/main.go
+++ webserver/main.go
@@ -9,6 +9,31 @@ import (
 	"path"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
 	port := flag.String("p", "8080", "port on which to serve web interfaces")
 	flag.Usage = usage
@@ -22,6 +47,8 @@ func main() {
 	snapdir := os.Getenv("SNAP")
 	www := path.Join(snapdir, flag.Arg(0))
 
+	http.HandleFunc("/view/", viewHandler)
+	
 	panic(http.ListenAndServe(":"+*port, http.FileServer(http.Dir(www))))
 }
 
